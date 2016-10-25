1354066441d7b7ede9b4f341109f803c1b9765ab
Mbadi Bayebec Prosper Emmanuel <mbadibayabec@gmail.com>
2016-10-25 13:46:15 +0200

    [try to see if i can display several things on the same page]

--- src/main.go
+++ src/main.go
@@ -6,14 +6,19 @@ import (
 )
 
 func main() {
+
+
   iris.UseTemplate(html.New(html.Config{Layout: "layouts/layout.html"}))
   iris.Get("/", accueil)
   iris.Get("/newUser", newUser)
   iris.Listen(":80")
 }
 
+
 func accueil(ctx *iris.Context) {
-  ctx.Render("accueil.html", nil)
+
+  ctx.Render("accueil.html", struct{ Name string }{Name: "iris1"}) 
+  ctx.Render("accueil.html", struct{ Name string }{Name: "iris2"})
 }
 
 func newUser(ctx *iris.Context) {
