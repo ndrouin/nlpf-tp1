4e97f0e5f2dee9c70ae78fabb55060c242db60dd
Nicolas Drouin <nicolas.drouin@epita.fr>
2016-10-25 03:57:50 +0200

    added layouts

--- src/main.go
+++ src/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/kataras/iris"
 
 func main() {
-
+  iris.UseTemplate(html.New(html.Config{Layout: "layouts/layout.html"}))
   iris.Get("/", newUser)
   iris.Listen(":80")
 }
