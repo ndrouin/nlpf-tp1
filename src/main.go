d2a8b3e4047c7b48538743e02f83aa6e8ff4e5eb
Mbadi Bayebec Prosper Emmanuel <mbadibayabec@gmail.com>
2016-10-25 15:17:59 +0200

    [suppression of the accueil layout ]

--- src/main.go
+++ src/main.go
@@ -17,10 +17,11 @@ func main() {
 
 func accueil(ctx *iris.Context) {
 
-ctx.Render("accueil/accueil_project_display.html", struct { Name string }{ Name:
-"iris1"},iris.RenderOptions{"layout":"layouts/accueil_layout.html"}) 
+ctx.Render("accueil/accueil_project_display.html", struct { Title string }{
+Name:"LES PROJECTS"}, struct {Name string}{Name:"Projet 1"} ) 
   
-ctx.Render("accueil/accueil_project_display.html", struct { Name string }{ Name: "iris2" }, iris.RenderOptions{"layout": iris.NoLayout})
+ctx.Render("accueil/accueil_project_display.html", struct { Name string }{
+Name:"Projet 2" }, iris.RenderOptions{"layout": iris.NoLayout})
 }
 
 func newUser(ctx *iris.Context) {
