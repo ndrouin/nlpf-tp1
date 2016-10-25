c24ba9f6c93daf1cda7870fa20ad5bd8d8f8a7e8
Nicolas Drouin <nicolas.drouin@epita.fr>
2016-10-25 03:01:43 +0200

    remove json part in main file

--- src/main.go
+++ src/main.go
@@ -8,13 +8,5 @@ func main(){
     ctx.Write("Hello, %s", "World!")
   })
 
-  iris.Get("/myjson", func(ctx *iris.Context){
-    ctx.JSON(iris.StatusOK, iris.Map{
-      "Name": "Iris",
-      "Released": "13 March 2016",
-      "Stars": 5525,
-    })
-  })
-
   iris.Listen(":80")
 }
