2d95f50321483b3de57a739dc8b2a368d346f835
prosper1994 <prosperemmanu.mbadibayebec@epita.fr>
2016-10-27 00:44:00 +0000

    [layout.html] Add of a header [connexion.html & newUser.html] Add of redirections

--- src/main.go
+++ src/main.go
@@ -23,22 +23,10 @@ func main() {
 func accueil(ctx *iris.Context) {
   //la liste de tous les projets 
 
-project1 := []string{"TEST1", "12/05/1994", "Ceci est la descriptionn de mon projet1", " 1euros"}
-project2 := []string{"TEST2", "13/06/2016", "Ceci est la descriptionn de mon projet2", "2 euros"}
-project3 := []string{"TEST3", "14/07/2017", "Ceci est la descriptionn de mon projet3", "3 euros"}
-project4 := []string{"TEST4", "15/08/2018", "Ceci est la descriptionn de mon projet4", "4 euros"}
-project5 := []string{"TEST5", "16/09/2019", "Ceci est la descriptionn de mon projet5", "5 euros"}
+c := []string{"TEST3", "14/07/2017", "Ceci est la descriptionn de mon projet3", "3 euros"}
 
-
-projects := [][]string{project1, project2, project3, project4, project5}
-//Affichage du tite de la page d'accueil"
-ctx.Render("Accueil/accueil_title.html", nil)
-
-
-for _,c:= range projects {
 ctx.Render("Accueil/accueil_projects.html", map[string]interface{}{"Projects": c[0], "Dates": c[1], "Descriptions": c[2],
-"Money":c[3]}, iris.RenderOptions{"layout": iris.NoLayout})}
-}
+"Money":c[3]})}
 
 func newUser(ctx *iris.Context) {
   ctx.Render("newUser.html", nil)
